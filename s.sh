4f62ae98f577e60eeb0144d7016ecf0c771199f7
UtkarshSiddhpura <utkarshsiddhpura09876@gmail.com>
2023-03-20 13:34:09 +0530

    add

--- s.sh
+++ s.sh
@@ -1,3 +1,3 @@
 git add .;
 git commit -m"add";
-git push https://UtkarshSiddhpura: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@Proposal-view.git origin --all;
+git push https://UtkarshSiddhpura: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@Proposal-view.git origin main;
